ed668ae4df67e3e3d3cdd112cf84f588bc73146c
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-04-11 12:40:59 -0500

    initial project

---
+++ app/src/main/java/com/manitos/dev/gilinhobakingapp/features/home/BakeListActivity.java
@@ -0,0 +1,96 @@
+package com.manitos.dev.gilinhobakingapp.features.home;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.manitos.dev.gilinhobakingapp.R;
+import com.manitos.dev.gilinhobakingapp.dummy.DummyContent;
+import com.manitos.dev.gilinhobakingapp.features.masterdetail.MasterDetailActivity;
+
+import java.util.List;
+
+public class BakeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bake_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        View recyclerView = findViewById(R.id.recycler_bake_list);
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        int totalCol = this.getResources().getInteger(R.integer.bake_list_num_col);
+        RecyclerView.LayoutManager lLayout = new GridLayoutManager(this, totalCol);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(lLayout);
+        recyclerView.setAdapter(new BakeListAdapter(DummyContent.ITEMS));
+    }
+
+    public static class BakeListAdapter
+            extends RecyclerView.Adapter<BakeListAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DummyContent.DummyItem item = (DummyContent.DummyItem) v.getTag();
+
+                Context context = v.getContext();
+                Intent intent = new Intent(context, MasterDetailActivity.class);
+                // intent.putExtra(BakeContainerFragment.ARG_ITEM_ID, item.id);
+
+                context.startActivity(intent);
+            }
+        };
+
+        public BakeListAdapter(List<DummyContent.DummyItem> mValues) {
+            this.mValues = mValues;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_bake_content, parent, false);
+
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.mTitle.setText(mValues.get(position).content);
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() { return mValues.size(); }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            final TextView mTitle;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                mTitle = (TextView) itemView.findViewById(R.id.item_bake_title);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/manitos/dev/gilinhobakingapp/features/masterdetail/MasterDetailActivity.java
@@ -0,0 +1,147 @@
+package com.manitos.dev.gilinhobakingapp.features.masterdetail;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.manitos.dev.gilinhobakingapp.features.bakedetail.BakeDetailActivity;
+import com.manitos.dev.gilinhobakingapp.features.components.BakeContainerFragment;
+import com.manitos.dev.gilinhobakingapp.R;
+import com.manitos.dev.gilinhobakingapp.dummy.DummyContent;
+
+import java.util.List;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link BakeDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class MasterDetailActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final MasterDetailActivity mParentActivity;
+        private final List<DummyContent.DummyItem> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(BakeContainerFragment.ARG_ITEM_ID, item.id);
+                    BakeContainerFragment fragment = new BakeContainerFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, BakeDetailActivity.class);
+                    intent.putExtra(BakeContainerFragment.ARG_ITEM_ID, item.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(MasterDetailActivity parent,
+                                      List<DummyContent.DummyItem> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+}
